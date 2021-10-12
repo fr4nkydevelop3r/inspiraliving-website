90a2c78b94fe67c326d7bbb390447652a8dda34c
Frank Andrade <fr4nky.develop3r@gmail.com>
2021-10-12 16:30:55 -0500

    testing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react'
 
 const Index = () => {
     return (
         <div>
-            <h1>Gatsby &amp; Contentful</h1>
+            <h1>Gatsby &amp; Contentful Blog</h1>
         </div>
-    );
-};
+    )
+}
 
-export default Index;
+export default Index
