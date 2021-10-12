1c36a49f185df0ca8a5cb0555fff856db3c6fe9c
Frank Andrade <fr4nky.develop3r@gmail.com>
2021-10-12 16:43:29 -0500

    testing 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Index = () => {
     return (
         <div>
-            <h1>Gatsby &amp; Contentful Blog</h1>
+            <h1>Gatsby &amp; Contentful what</h1>
         </div>
     )
 }
